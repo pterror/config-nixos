69212db7f5b27a2cede8196c2616bfb2592230b5
pterror <pterrorbird@gmail.com>
2024-11-02 05:45:33 +1000

    move substituters to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,19 @@
   imports = [ ./hardware-configuration.nix ./cachix.nix inputs.home-manager.nixosModules.home-manager ];
   documentation.nixos.enable = false;
 
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://stardustxr.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "stardustxr.cachix.org-1:mWSn8Ap2RLsIWT/8gsj+VfbJB6xoOkPaZpbjO+r9HBo="
+    ];
+  };
+
   environment.persistence = {
     "/persistent" = {
       hideMounts = true;
--- flake.nix
+++ flake.nix
@@ -1,14 +1,4 @@
 {
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://nix-gaming.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     impermanence.url = github:nix-community/impermanence;
